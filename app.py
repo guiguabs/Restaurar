0f3aa1cde4d8a8f08a7baf7fcb56ce90320670c0
guiguabs <guiguabs@gmail.com>
2026-09-11 17:45:28 -0300

    Update app.py

--- app.py
+++ app.py
@@ -3,8 +3,7 @@ from flask_sqlalchemy import SQLAlchemy
 app = Flask(__name__, template_folder='.')
 
 # Chave secreta obrigatória para usar sessões no Flask
-app = Flask(__name__)
-
+app = Flask(__name__, template_folder='menu')
 # Configuração do Banco de Dados SQLite
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
