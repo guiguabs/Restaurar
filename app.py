1b9ec8ab302952d714f43e6a0f889d0b9f8d9a4c
guiguabs <guiguabs@gmail.com>
2026-09-11 17:59:08 -0300

    Update app.py

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ from flask import Flask, render_template, request, redirect, url_for, session
 from flask_sqlalchemy import SQLAlchemy
 
 # Chave secreta obrigatória para usar sessões no Flask
-app = Flask(__name__, template_folder='menu')
+app = Flask(__name__)
 # Configuração do Banco de Dados SQLite
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
